960f584170b27fef459155264e9e55df113d9085
vinay-83 <kpit@L-9927>
2020-09-11 15:49:35 +0530

    Initial commit

---
+++ src/main/java/com/isg/unipro/K8sAppUniproApplication.java
@@ -0,0 +1,13 @@
+package com.isg.unipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sAppUniproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sAppUniproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isg/unipro/K8sAppUniproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isg.unipro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAppUniproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
